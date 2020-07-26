a146c31fccafc68737ba311ba08640b1a230c3c5
Lexou <lexou-dit-tuc@hotmail.fr>
2020-07-26 03:56:10 +0200

    fix: fixed erroneous includes in libft.h, made t_bool the native _Bool boolean type + added t_ptrdiff

--- src/memory/ft_memswap.c
+++ src/memory/ft_memswap.c
@@ -24,13 +24,13 @@ int			ft_memswap(void *ptr1, void *ptr2, t_size size)
 #endif
 	if (ptr1 == ptr2)
 		return (OK);
-	else if (size == 1 && *(uint8_t*)(ptr1) == *(uint8_t*)(ptr2))
+	else if (size == 1 && *(t_u8*)(ptr1) == *(t_u8*)(ptr2))
 		return (OK);
-	else if (size == 2 && *(uint16_t*)(ptr1) == *(uint16_t*)(ptr2))
+	else if (size == 2 && *(t_u16*)(ptr1) == *(t_u16*)(ptr2))
 		return (OK);
-	else if (size == 4 && *(uint32_t*)(ptr1) == *(uint32_t*)(ptr2))
+	else if (size == 4 && *(t_u32*)(ptr1) == *(t_u32*)(ptr2))
 		return (OK);
-	else if (size == 8 && *(uint64_t*)(ptr1) == *(uint64_t*)(ptr2))
+	else if (size == 8 && *(t_u64*)(ptr1) == *(t_u64*)(ptr2))
 		return (OK);
 	else if (ptr1 && ptr2)
 	{
