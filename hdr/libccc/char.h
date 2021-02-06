f34b5f987c671ef1ec40cbc2f53a961eb44a57e9
lexouduck <lexou-dit-tuc@hotmail.fr>
2021-02-07 00:07:12 +0100

    feature(libccc/char): implemented UTF8_Get() function which returns a UTF32 code point from a string

--- hdr/libccc/char.h
+++ hdr/libccc/char.h
@@ -27,6 +27,7 @@
 #include "libccc_define.h"
 
 #include "libccc/bool.h"
+#include "libccc/int.h"
 
 HEADER_CPP
 
@@ -36,13 +37,21 @@ HEADER_CPP
 ** ************************************************************************** *|
 */
 
-//! The primitive `char` text character type
+//! The primitive `char` text character type (1 byte: ANSI/ASCII)
 typedef char	t_char;
 TYPEDEF_ALIAS(	t_char, CHAR, PRIMITIVE)
 
-//! The primitive multi-byte type for a Unicode character
-typedef int		t_utf8;
-TYPEDEF_ALIAS(	t_utf8, UTF8, PRIMITIVE)
+//! The primitive type for a Unicode UTF-8 character (variable length: 1 to 4 bytes)
+typedef char			t_utf8;
+TYPEDEF_ALIAS(			t_utf8, UTF8, PRIMITIVE)
+
+//! The primitive type for a Unicode UTF-16 character (variable length: 2 or 4 bytes)
+typedef uint_least16_t	t_utf16;
+TYPEDEF_ALIAS(			t_utf16, UTF16, PRIMITIVE)
+
+//! The primitive type for a Unicode UTF-32 character (4-byte) ie: a Unicode code point value
+typedef uint_least32_t	t_utf32;
+TYPEDEF_ALIAS(			t_utf32, UTF32, PRIMITIVE)
 
 
 
@@ -85,111 +94,123 @@ TYPEDEF_ALIAS(	t_utf8, UTF8, PRIMITIVE)
 /*!
 **	Check if the given char 'c' is a letter (lowercase or uppercase)
 **
-**	@param	c	The character to check (UTF-8)
+**	@param	c	The character to check (Unicode)
 **	@returns 1(TRUE) if the given char 'c' is a letter (lowercase or uppercase),
 **			and returns 0(FALSE) otherwise.
 */
-t_bool				Char_IsLetter(int c);
+_INLINE()
+t_bool				Char_IsLetter(t_utf32 c);
 #define c_isalpha	Char_IsLetter
 
 //! Check if the given char 'c' is a uppercase letter character
 /*!
 **	Check if the given char 'c' is a uppercase letter character
 **
-**	@param	c	The character to check (UTF-8)
+**	@param	c	The character to check (Unicode)
 **	@returns 1(TRUE) if the given char 'c' is a uppercase letter character,
 **			and returns 0(FALSE) otherwise.
 */
-t_bool				Char_IsUppercase(int c);
+_INLINE()
+t_bool				Char_IsUppercase(t_utf32 c);
 #define c_isupper	Char_IsUppercase
 
 //! Check if the given char 'c' is an lowercase letter character
 /*!
 **	Check if the given char 'c' is an lowercase letter character
 **
-**	@param	c	The character to check (UTF-8)
+**	@param	c	The character to check (Unicode)
 **	@returns 1(TRUE) if the given char 'c' is an lowercase letter character,
 **			and returns 0(FALSE) otherwise.
 */
-t_bool				Char_IsLowercase(int c);
+_INLINE()
+t_bool				Char_IsLowercase(t_utf32 c);
 #define c_islower	Char_IsLowercase
 
 //! Check if the given char 'c' is alphanumeric (letter or digit) character
 /*!
 **	Check if the given char 'c' is alphanumeric (letter or digit) character
 **
-**	@param	c	The character to check (UTF-8)
+**	@param	c	The character to check (Unicode)
 **	@returns 1(TRUE) if the given char 'c' is alphanumeric (letter or digit),
 **			and returns 0(FALSE) otherwise.
 */
-t_bool				Char_IsAlphaNumeric(int c);
+_INLINE()
+t_bool				Char_IsAlphaNumeric(t_utf32 c);
 #define c_isalnum	Char_IsAlphaNumeric
 
 //! Check if the given char 'c' is a numerical digit character
 /*!
 **	Check if the given char 'c' is a numerical digit character
 **
-**	@param	c	The character to check (UTF-8)
+**	@param	c	The character to check (Unicode)
 **	@returns 1(TRUE) if the given char 'c' is a numerical digit character,
 **			and returns 0(FALSE) otherwise.
 */
-t_bool				Char_IsDigit(int c);
+_INLINE()
+t_bool				Char_IsDigit(t_utf32 c);
 #define c_isdigit	Char_IsDigit
 
 //! Check if the given char 'c' is a hexadecimal character
 /*!
 **	Check if the given char 'c' is a hexadecimal character
 **
-**	@param	c	The character to check (UTF-8)
+**	@param	c	The character to check (Unicode)
 **	@returns 1(TRUE) if the given char 'c' is a hexadecimal character
 **			(either 0-9 or  A-F lowercase or uppercase), and returns 0(FALSE) otherwise.
 */
-t_bool				Char_IsHexDigit(int c);
+_INLINE()
+t_bool					Char_IsHexDigit(t_utf32 c);
 #define c_ishexdigit	Char_IsHexDigit
 
 //! Check if the given char 'c' is whitespace character (\r,\n,\f,\v,\t,' ')
 /*!
 **	Check if the given char 'c' is whitespace character (\r,\n,\f,\v,\t,' ')
 **
-**	@param	c	The character to check (UTF-8)
+**	@param	c	The character to check (Unicode)
 **	@returns 1(TRUE) if the given char 'c' is whitespace character: \r,\n,\f,\v,\t,' '
 **			(carriage return, newline, form feed, vertical tab, tab, or space), and returns 0(FALSE) otherwise.
 */
-t_bool				Char_IsSpace(int c);
+_INLINE()
+t_bool				Char_IsSpace(t_utf32 c);
 #define c_isspace	Char_IsSpace
 
 //! Check if the given char 'c' is a punctuation character
 /*!
 **	Check if the given char 'c' is a punctuation character
 **
-**	@param	c	The character to check (UTF-8)
+**	@param	c	The character to check (Unicode)
 **	@returns 1(TRUE) if the given char 'c' is a punctuation character,
 **			and returns 0(FALSE) otherwise.
 */
-t_bool				Char_IsPunctuation(int c);
+_INLINE()
+t_bool				Char_IsPunctuation(t_utf32 c);
 #define c_ispunct	Char_IsPunctuation
 
 //! Check if the given char 'c' is a printable ASCII character
 /*!
 **	Check if the given char 'c' is a printable ASCII character
 **
-**	@param	c	The character to check (UTF-8)
+**	@param	c	The character to check (Unicode)
 **	@returns 1(TRUE) if the given char 'c' is a printable ASCII character,
 **			(0x20 <= c && c <= 0x7E) and returns 0(FALSE) otherwise.
 */
-t_bool				Char_IsPrintable(int c);
+_INLINE()
+t_bool				Char_IsPrintable(t_utf32 c);
 #define c_isprint	Char_IsPrintable
 
-//! Check if the given char 'c' is an ASCII-compatible char (non UTF-8)
+//! Check if the given char 'c' is an ASCII-compatible char (non-Unicode)
 /*!
-**	Check if the given char 'c' is an ASCII-compatible char (non UTF-8)
+**	Check if the given char 'c' is an ASCII-compatible char (non-Unicode)
 **
-**	@param	c	The character to check (UTF-8)
+**	@param	c	The character to check (Unicode)
 **	@returns 1(TRUE) if the given char 'c' has a value between 0 and 127,
 **			(0x00 <= c && c <= 0x7F) and returns 0(FALSE) otherwise.
 */
-t_bool				Char_IsASCII(int c);
+_INLINE()
+t_bool				Char_IsASCII(t_utf32 c);
 #define c_isascii	Char_IsASCII
+
+
 
 
 
@@ -203,11 +224,12 @@ t_bool				Char_IsASCII(int c);
 /*!
 **	Get the uppercase version of the given char 'c'
 **
-**	@param	c	The character to uppercase-ify (UTF-8)
+**	@param	c	The character to uppercase-ify (Unicode)
 **	@returns the uppercase equivalent of the given character 'c'
 **			(nothing is done if 'c' is not a letter character).
 */
-char					Char_ToUppercase(char c);
+_INLINE()
+t_char					Char_ToUppercase(t_utf32 c);
 #define c_toupper		Char_ToUppercase
 #define Char_ToUpper	Char_ToUppercase
 
@@ -215,16 +237,28 @@ char					Char_ToUppercase(char c);
 /*!
 **	Get the lowercase version of the given char 'c'
 **
-**	@param	c	The character to lowercase-ify (UTF-8)
+**	@param	c	The character to lowercase-ify (Unicode)
 **	@returns the lowercase equivalent of the given character 'c'
 **			(nothing is done if 'c' is not a letter character).
 */
-char					Char_ToLowercase(char c);
+_INLINE()
+t_char					Char_ToLowercase(t_utf32 c);
 #define c_tolower		Char_ToLowercase
 #define Char_ToLower	Char_ToLowercase
 
 // TODO add Char_ToMixedCase
 
+
+
+/*
+** ************************************************************************** *|
+**                             Encoding Conversions                           *|
+** ************************************************************************** *|
+*/
+
+//! Returns the Unicode code point (UTF-32) for the character pointed to by 'str'
+t_utf32					UTF8_Get(t_utf8 const* str);
+#define c_utf8get		UTF8_Get
 
 
 /*! @} */
---
+++ src/char/utf8.c
@@ -0,0 +1,56 @@
+
+#include "libccc/char.h"
+#include "libccc/pointer.h"
+
+
+
+inline t_utf32		UTF8_Get(t_utf8 const* str)
+{
+	static const t_u8 MASK = ((1 << 6) - 1);
+	t_u8 c;
+
+#if LIBCONFIG_HANDLE_NULLPOINTERS
+	if (str == NULL)
+		return (ERROR);
+#endif
+	c = str[0];
+	if (c & (1 << 7)) // multi-byte character
+	{
+		t_utf32 result = 0;
+		t_u8 mask;
+		if (c & (1 << 6)) // 2-byte character
+		{
+			if (c & (1 << 5)) // 3-byte character
+			{
+				if (c & (1 << 4)) // 4-byte character
+				{
+					if (c & (1 << 3))
+						return ((t_utf32)c); // INVALID UTF-8
+					mask = ((1 << 3) - 1);
+					result |= (c & mask) << (6 * 3);	c = str[1];
+					result |= (c & MASK) << (6 * 2);	c = str[2];
+					result |= (c & MASK) << (6 * 1);	c = str[3];
+					result |= (c & MASK);
+					return (result);
+				}
+				else
+				{
+					mask = ((1 << 4) - 1);
+					result |= (c & mask) << (6 * 2);	c = str[1];
+					result |= (c & MASK) << (6 * 1);	c = str[2];
+					result |= (c & MASK);
+					return (result);
+				}
+			}
+			else
+			{
+				mask = ((1 << 5) - 1);
+				result |= (c & mask) << (6 * 1);	c = str[1];
+				result |= (c & MASK);
+				return (result);
+			}
+		}
+		else return ((t_utf32)c); // INVALID UTF-8
+	}
+	else return ((t_utf32)c);
+}
