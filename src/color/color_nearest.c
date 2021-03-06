dd18baeb3da9adcfcd021c5f027d861233c4fd65
lexouduck <lexou-dit-tuc@hotmail.fr>
2021-03-06 03:48:47 +0100

    fix(libccc/color): fixed problem in GetNearest() functions

--- src/color/color_nearest.c
+++ src/color/color_nearest.c
@@ -50,7 +50,7 @@ t_argb32 const*		Color_ARGB32_GetNearest(
 	result = NULL;
 	while (n--)
 	{
-		diff = Color_ARGB16_Difference(target, colors[n]);
+		diff = Color_ARGB32_Difference(target, colors[n]);
 		if (diff < min_diff)
 		{
 			min_diff = diff;
@@ -67,29 +67,24 @@ s_argb const*		Color_ARGB_GetNearest(
 	s_argb const* colors,
 	t_size n)
 {
-	t_s16 r;
-	t_s16 g;
-	t_s16 b;
-	t_s16 min_diff;
+	t_u32 diff;
+	t_u32 min_diff;
 	s_argb const* result;
 
 #if LIBCONFIG_HANDLE_NULLPOINTERS
 	if (colors == NULL)
 		return (NULL);
 #endif
-	min_diff = 3.0;
+	min_diff = U32_MAX;
 	result = NULL;
 	while (n--)
 	{
-		r = target.r - colors[n].r;
-		g = target.g - colors[n].g;
-		b = target.b - colors[n].b;
-		r = ABS(r);
-		g = ABS(g);
-		b = ABS(b);
-		if ((r + g + b) < min_diff)
+		diff = Color_ARGB32_Difference(
+			Color_ARGB_To_ARGB32(&target),
+			Color_ARGB_To_ARGB32(&colors[n]));
+		if (diff < min_diff)
 		{
-			min_diff = (r + g + b);
+			min_diff = diff;
 			result = &colors[n];
 		}
 	}
