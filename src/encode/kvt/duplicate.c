48f1516831b7b81db045f35cbdd1de2fa3641e7f
lexouduck <lexou-dit-tuc@hotmail.fr>
2021-05-21 06:02:17 +0200

    fix(libccc/encode/common): fixed KVT_Duplicate() function which was missing important type checks

--- src/encode/kvt/duplicate.c
+++ src/encode/kvt/duplicate.c
@@ -1,6 +1,7 @@
 
 #include "libccc.h"
 #include "libccc/char.h"
+#include "libccc/memory.h"
 #include "libccc/string.h"
 #include "libccc/encode/common.h"
 
@@ -9,28 +10,37 @@
 s_kvt*	KVT_Duplicate(s_kvt const* item, t_bool recurse)
 {
 	s_kvt* newitem = NULL;
-	s_kvt* child = NULL;
-	s_kvt* next = NULL;
 	s_kvt* newchild = NULL;
+	s_kvt* next = NULL;
+	s_kvt* child = NULL;
+	t_utf8* tmp = NULL;
 
 	// Bail on bad ptr
-	if (!item)
+	if (item == NULL)
 	{
 		goto failure;
 	}
 	// Create new item
 	newitem = KVT_Item();
-	if (!newitem)
+	if (newitem == NULL)
 	{
 		goto failure;
 	}
 	// Copy over all vars
 	newitem->type = item->type & (~DYNAMICTYPE_ISREFERENCE);
 	newitem->value.number = item->value.number;
-	if (item->value.string)
+	if (item->type & DYNAMICTYPE_NULL)		Memory_Clear(&newitem->value, sizeof(union dynamic));
+	if (item->type & DYNAMICTYPE_BOOLEAN)	newitem->value.boolean = item->value.boolean;
+	if (item->type & DYNAMICTYPE_INTEGER)	newitem->value.integer = item->value.integer;
+	if (item->type & DYNAMICTYPE_FLOAT)		newitem->value.number = item->value.number;
+	if (item->type & DYNAMICTYPE_STRING)	tmp = item->value.string;
+	if (item->type & DYNAMICTYPE_RAW)		tmp = item->value.string;
+	if (item->type & DYNAMICTYPE_ARRAY)		newitem->value.child = NULL;
+	if (item->type & DYNAMICTYPE_OBJECT)	newitem->value.child = NULL;
+	if (tmp)
 	{
 		newitem->value.string = (t_char*)String_Duplicate((t_char*)item->value.string);
-		if (!newitem->value.string)
+		if (newitem->value.string == NULL)
 		{
 			goto failure;
 		}
@@ -38,7 +48,7 @@ s_kvt*	KVT_Duplicate(s_kvt const* item, t_bool recurse)
 	if (item->key)
 	{
 		newitem->key = (t_char*)String_Duplicate((t_char*)item->key);
-		if (!newitem->key)
+		if (newitem->key == NULL)
 		{
 			goto failure;
 		}
@@ -47,11 +57,12 @@ s_kvt*	KVT_Duplicate(s_kvt const* item, t_bool recurse)
 	if (!recurse)
 		return (newitem);
 	// Walk the ->next chain for the child.
-	child = item->value.child;
+	if (item->type & DYNAMICTYPE_ARRAY)		child = item->value.child;
+	if (item->type & DYNAMICTYPE_OBJECT)	child = item->value.child;
 	while (child != NULL)
 	{
 		newchild = KVT_Duplicate(child, TRUE); // Duplicate (with recurse) each item in the ->next chain
-		if (!newchild)
+		if (newchild == NULL)
 		{
 			goto failure;
 		}
