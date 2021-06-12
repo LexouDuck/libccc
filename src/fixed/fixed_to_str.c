f9ac5bebdcf1941853bb93c34795181fbcfa0da7
LexouDuck <lexouduck@gmail.com>
2021-06-12 19:45:43 +0200

    fix(libccc/fixed): fixed Fixed_ToString() functions, to avoid using String_Format()

--- src/fixed/fixed_to_str.c
+++ src/fixed/fixed_to_str.c
@@ -1,22 +1,40 @@
 
 #include "libccc/fixed.h"
+#include "libccc/int.h"
 #include "libccc/string.h"
-#include "libccc/format.h"
 
 #include LIBCONFIG_ERROR_INCLUDE
 
 
 
 #define DEFINEFUNC_FIXED_TOSTR(BITS) \
-inline t_char*	Q##BITS##_ToString(t_q##BITS number)	\
+t_char*	Q##BITS##_ToString(t_q##BITS number)			\
 {														\
+	t_s##BITS	n;										\
+	t_char*	result = NULL;								\
+	t_char*	tmp = NULL;									\
+														\
+	n = Q##BITS##_IntegerPart(number);					\
+	tmp = S##BITS##_ToString(n);						\
+	result = String_Append(&tmp, ".(");					\
+	n = Q##BITS##_FractionPart(number);					\
+	tmp = S##BITS##_ToString(n);						\
+	result = String_Merge(&result, &tmp);				\
+	result = String_Append(&result, "/");				\
+	n = FIXED_DENOMINATOR;								\
+	tmp = S##BITS##_ToString(n);						\
+	result = String_Merge(&result, &tmp);				\
+	result = String_Append(&result, ")");				\
+	return (result);									\
+}
+/*
 	return (String_Format(								\
 		SF_S##BITS".("SF_S##BITS"/"SF_S##BITS")",		\
 		(t_s##BITS)Q##BITS##_IntegerPart(number),		\
 		(t_s##BITS)Q##BITS##_FractionPart(number),		\
 		(t_s##BITS)(FIXED_DENOMINATOR))					\
 	);													\
-}														\
+*/
 
 DEFINEFUNC_FIXED_TOSTR(16)
 DEFINEFUNC_FIXED_TOSTR(32)
