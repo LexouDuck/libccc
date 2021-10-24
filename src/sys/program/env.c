476807f7c7ee8a4f2b653cfed7b93bcd36f8329b
LexouDuck <lexouduck@gmail.com>
2021-10-24 02:52:42 +0200

    fix(libccc/sys/program/env): compatibility fix, using fallback with 'putenv' if 'setenv' is not available

--- src/sys/program/env.c
+++ src/sys/program/env.c
@@ -5,7 +5,11 @@
 #else
 	char*	getenv(char const* name);
 	errno_t getenv_s(size_t* len, char* value, rsize_t valuesz, char const* name);
-	int		setenv(char const* name, char const* value, int overwrite);
+	#if (_POSIX_C_SOURCE >= 200112L || _BSD_SOURCE)
+		int	setenv(char const* name, char const* value, int overwrite);
+	#else
+		int	putenv(t_char const* command);
+	#endif
 #endif
 
 #include "libccc/sys/program.h"
@@ -28,8 +32,21 @@ t_char*	Program_GetEnv(t_char const* name)
 
 e_cccerror	Program_SetEnv(t_char const* name, t_char const* value, t_bool overwrite)
 {
+#if (_POSIX_C_SOURCE >= 200112L || _BSD_SOURCE)
 	HANDLE_ERROR(SYSTEM,
 		setenv(name, value, overwrite),
 		return (ERROR_SYSTEM);)
+#else
+	if (!overwrite)
+	{
+		if (Program_GetEnv(name) != NULL)
+			return (OK);
+	}
+	t_char* command = String_Format("%s=%s", name, value);
+	HANDLE_ERROR(SYSTEM,
+		putenv(command),
+		return (ERROR_SYSTEM);)
+	String_Delete(&command);
+#endif
 	return (OK);
 }
