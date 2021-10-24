8fffcf51ac77b80c38da66d8ce7fd5526304991d
LexouDuck <lexouduck@gmail.com>
2021-10-24 14:55:45 +0200

    feature,wip(Makefile): added bogus 'make format' rule, and started implementing 'make update-submodules' rule

--- make/rules/update.mk
+++ make/rules/update.mk
@@ -13,3 +13,19 @@ update-lists:
 
 
 # TODO: package manager update logic (for submodules, and wget/curl packages)
+#
+#GIT_SUBMODULES = $(shell cat .gitmodules | grep "\[submodule" | cut -d'"' -f 2)
+#
+#.PHONY:\
+#update-submodules # Updates git submodules
+#update-submodules:
+#	@for i in $(GIT_SUBMODULES) ; do \
+#		cd $$i ; \
+#		if git status | grep -q "HEAD detached" ; then \
+#			printf $(C_YELLOW)"WARNING"$(C_RESET)": Your git submodule "$$i" is in detached HEAD state""\n" ; \
+#			printf "You need to manually go into the submodule folder and do 'git checkout master',""\n" ; \
+#			printf "after making sure that you have no uncommitted/unpushed local working changes.""\n" ; \
+#		fi ; \
+#		git pull ; \
+#		cd - ; \
+#	done
