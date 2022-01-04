e6f97797828b4503136ec2b5436c6dc455d05d13
LexouDuck <lexouduck@gmail.com>
2022-01-04 05:27:11 +0100

    improvement(Makefile): added utility variables to make/utils/print.mk

--- make/utils/print.mk
+++ make/utils/print.mk
@@ -10,3 +10,22 @@ print_warning = printf $(IO_YELLOW)"Warning"$(IO_RESET)": "$(1)"\n"
 print_success = printf $(IO_GREEN)"Success"$(IO_RESET)": "$(1)"\n"
 #! The shell command to print a makefile info message (one parameter: the message)
 print_message = printf $(IO_CYAN)$(1)$(IO_RESET)"\n"
+
+
+
+#! These variables are useful in certain contexts where escapes dont work as usual
+ifneq ($(findstring MINGW,$(UNAME_S)),)
+	# shell is MINGW (for MINGW64_NT and MINGW32_NT)
+	comma	= ,
+	tab 	= \\\\t
+	nl		= \\\\n
+	squote	= \\\'
+	dquote	= \\\"
+else
+	# shell is normal
+	comma	= ,
+	tab 	= \\t
+	nl		= \\n
+	squote	= \'
+	dquote	= \"
+endif
