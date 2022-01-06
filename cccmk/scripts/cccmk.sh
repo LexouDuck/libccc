3fef1e68b6f7e64ae7b8914f5c5395eed2741221
LexouDuck <lexouduck@gmail.com>
2022-01-06 05:01:21 +0100

    fix(cccmk): fixed 'cccmk update' using paths relative to ./mkfile folder rather than ./ project root

--- cccmk/scripts/cccmk.sh
+++ cccmk/scripts/cccmk.sh
@@ -294,18 +294,20 @@ case "$command" in
 	update)
 		for i in $command_arg_path
 		do
-			if ! [ -f "./$project_mkpath/$i" ]
+			file_pwd="$i"
+			file_ccc="$CCCMK_PATH_MKFILES/$i"
+			file_ccc=`echo "$file_ccc" | sed "s|$project_mkpath/||g"`
+			if ! [ -f "$file_pwd" ]
 			then
-				print_error "Invalid filepath for 'cccmk update': ./$project_mkpath/$i"
-				print_error "The path should be relative to the '$project_mkpath' folder"
+				print_error "Invalid filepath given to 'cccmk update': '$file_pwd' - no such file exists."
 				exit 1
 			fi
-			if ! cmp "$CCCMK_PATH_MKFILES/$i" "./$project_mkpath/$i"
+			if ! cmp "$file_ccc" "$file_pwd"
 			then
-				cccmk_diff "$CCCMK_PATH_MKFILES/$i" "./$project_mkpath/$i"
-				print_message "The file './$project_mkpath/$i' differs from the cccmk template (see diff above)"
-				print_message "Your file is shown as old/red, and the latest cccmk template is shown as new/green."
-				read -p "Are you sure you wish to overwrite './$project_mkpath/$i' ? [y/N]" response
+				cccmk_diff "$file_ccc" "$file_pwd"
+				print_message "The file '$file_pwd' differs from the cccmk template (see diff above)"
+				print_message "NOTE: your file is shown as old/red, and the latest cccmk template is shown as new/green."
+				read -p "Are you sure you wish to overwrite '$file_pwd' ? [y/N]" response
 				response=`echo "$response" | tr [:upper:] [:lower:]` # force lowercase
 				case $response in
 					y|ye|yes) response=true ;;
@@ -314,12 +316,12 @@ case "$command" in
 				esac
 			else
 				response=false
-				print_message "The file './$project_mkpath/$i' is identical to the latest cccmk template."
+				print_message "The file '$file_pwd' is identical to the latest cccmk template."
 			fi
 			if $response
 			then
-				print_message "Updating file './$project_mkpath/$i'..."
-				cp "$CCCMK_PATH_MKFILES/$i" "./$project_mkpath/$i"
+				print_message "Updating file '$file_pwd'..."
+				cp "$file_ccc" "$file_pwd"
 			fi
 		done
 		;;
