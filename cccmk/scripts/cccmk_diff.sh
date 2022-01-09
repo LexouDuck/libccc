718bf3c1d580b209a2d34687c3ebebc16b029b88
LexouDuck <lexouduck@gmail.com>
2022-01-09 16:59:11 +0100

    improvement(cccmk): various fixes and improvement to 'cccmk diff' command

--- cccmk/scripts/cccmk_diff.sh
+++ cccmk/scripts/cccmk_diff.sh
@@ -7,52 +7,49 @@ then print_error "The current folder is not a valid cccmk project folder (needed
 exit 1
 fi
 
+print_message "folder differences:"
+
 #! temporary folder used to check differences with source cccmk templates
 diffchk_mkpath="$project_mkpath.tmp"
+mkdir "$command_arg_path/$diffchk_mkpath"
+# iterate over all mkfile folders
+for dir in `list_subfolders "$CCCMK_PATH_MKFILES"`
+do
+	# copy over all files
+	cp -r "$CCCMK_PATH_MKFILES/$dir" "$command_arg_path/$diffchk_mkpath/$dir"
+	# iterate over all subfolders, and check '_if_*' folders to conditionally copy certain scripts
+	for subdir in `list_subfolders "$diffchk_mkpath/$dir"`
+	do
+		case "$subdir" in
+			_if_selected) # prompt the user to select which scripts they want
+				proposed_scripts=`ls "$diffchk_mkpath/$dir/$subdir/" | sort --ignore-case | xargs`
+				for i in $proposed_scripts
+				do
+					if contains "$project_scripts" "$project_mkpath/$dir/$i"
+					then
+						mv  "$command_arg_path/$diffchk_mkpath/$dir/$subdir/$i" \
+							"$command_arg_path/$diffchk_mkpath/$dir/$i"
+					fi
+				done
+				;;
+			_if_type_*) # only copy over files if $project_type matches folder name part after '_if_type_'
+				if [ "$subdir" == "_if_type_$project_type" ]
+				then
+					mv  "$command_arg_path/$diffchk_mkpath/$dir/$subdir/"* \
+						"$command_arg_path/$diffchk_mkpath/$dir/"
+				fi
+				;;
+			*)	break
+				;;
+		esac
+	done
+	# cleanup up leftover '_if_*' folders
+	rm -rf "./$diffchk_mkpath/$dir/"_if_*
+done
 
-print_message "folder differences:"
-#folder_diff=`diff -qsr "$CCCMK_PATH_MKFILES" "$command_arg_path/$project_mkpath" || echo ''`
+# show mkfile folder tree differences
 if tree --version > /dev/null
 then
-	mkdir "$command_arg_path/$diffchk_mkpath"
-	# iterate over all mkfile folders
-	for dir in `list_subfolders "$CCCMK_PATH_MKFILES"`
-	do
-		# copy over all files
-		cp -r "$CCCMK_PATH_MKFILES/$dir" "$command_arg_path/$diffchk_mkpath/$dir"
-		# iterate over all subfolders, and check '_if_*' folders to conditionally copy certain scripts
-		for subdir in `list_subfolders "$diffchk_mkpath/$dir"`
-		do
-			case "$subdir" in
-				_if_selected) # prompt the user to select which scripts they want
-					proposed_scripts=`ls "$diffchk_mkpath/$dir/$subdir/" | sort --ignore-case | xargs`
-					for i in $proposed_scripts
-					do
-						if contains "$project_scripts" "$project_mkpath/$dir/$i"
-						then
-							mv  "$command_arg_path/$diffchk_mkpath/$dir/$subdir/$i" \
-								"$command_arg_path/$diffchk_mkpath/$dir/$i"
-						fi
-					done
-					;;
-
-
-
-
-				_if_type_*) # only copy over files if $project_type matches folder name part after '_if_type_'
-					if [ "$subdir" == "_if_type_$project_type" ]
-					then
-						mv  "$command_arg_path/$diffchk_mkpath/$dir/$subdir/"* \
-							"$command_arg_path/$diffchk_mkpath/$dir/"
-					fi
-					;;
-				*)	break
-					;;
-			esac
-		done
-		# cleanup up leftover '_if_*' folders
-		rm -rf "./$diffchk_mkpath/$dir/"_if_*
-	done
 	tree -L 2 -a "$command_arg_path/$diffchk_mkpath" > ./cccmk_tree_tmp.txt
 	tree -L 2 -a "$command_arg_path/$project_mkpath" > ./cccmk_tree_cwd.txt
 	diff -U-1 \
@@ -92,6 +89,7 @@ else
 	print_warning "This computer has no 'tree' command installed, cannot display folder tree diff"
 fi
 
+# if verbose, show diffs for each non-identical file
 if $verbose
 then
 	for i in $project_scripts
