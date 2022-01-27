1dd0ea85ed3d6cc92f0800c102846bbb666f9b13
LexouDuck <lexouduck@gmail.com>
2022-01-27 21:03:26 +0100

    update,fix(cccmk): harmonized libccc mkfile scripts with the cccmk template project, using the new if ocnditional directives

--- cccmk/project/mkfile/rules/prereq.mk
+++ cccmk/project/mkfile/rules/prereq.mk
@@ -54,7 +54,7 @@ endif
 prereq #! Checks all prerequisite tools/programs and their versions
 prereq: init \
 	prereq-build \
-%%if tracked(mkfile/rules/_if_multiselect/tests.mk)    :	prereq-tests \
+%%if tracked(mkfile/rules/_if_flag_testsuite/test.mk)  :	prereq-tests \
 %%if tracked(mkfile/rules/_if_multiselect/coverage.mk) :	prereq-coverage \
 %%if tracked(mkfile/rules/_if_multiselect/format.mk)   :	prereq-format \
 %%if tracked(mkfile/rules/_if_multiselect/lint.mk)     :	prereq-lint \
