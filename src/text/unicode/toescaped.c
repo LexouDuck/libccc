8e347ee3a9ecad9dc80ed7a008fc6b2019c7c9c0
Yanis-F <yanis.fourel@ledr.io>
2022-05-05 12:50:39 +0200

    format(libccc/text/unicode: rename 'hexa' to 'nibble' in UTF32 escape

--- src/text/unicode/toescaped.c
+++ src/text/unicode/toescaped.c
@@ -15,13 +15,13 @@ t_size UTF32_ToEscaped_xFF(t_char *dest, t_utf32 c)
 
 	if (dest)
 	{
-		t_u8 hexa0 = (c & 0x0F) >> 0;
-		t_u8 hexa1 = (c & 0xF0) >> 4;
+		t_u8 nibble0 = (c & 0x0F) >> 0;
+		t_u8 nibble1 = (c & 0xF0) >> 4;
 
 		dest[0] = '\\';
 		dest[1] = 'x';
-		dest[2] = (hexa1 < 10) ? (hexa1 + '0') : (hexa1 - 10 + 'A');
-		dest[3] = (hexa0 < 10) ? (hexa0 + '0') : (hexa0 - 10 + 'A');
+		dest[2] = (nibble1 < 10) ? (nibble1 + '0') : (nibble1 - 10 + 'A');
+		dest[3] = (nibble0 < 10) ? (nibble0 + '0') : (nibble0 - 10 + 'A');
 	}
 	return 4;
 }
@@ -33,17 +33,17 @@ t_size UTF32_ToEscaped_uFFFF(t_char *dest, t_utf32 c)
 
 	if (dest)
 	{
-		t_u8 hexa0 = (c & 0x000F) >> 0;
-		t_u8 hexa1 = (c & 0x00F0) >> 4;
-		t_u8 hexa2 = (c & 0x0F00) >> 8;
-		t_u8 hexa3 = (c & 0xF000) >> 12;
+		t_u8 nibble0 = (c & 0x000F) >> 0;
+		t_u8 nibble1 = (c & 0x00F0) >> 4;
+		t_u8 nibble2 = (c & 0x0F00) >> 8;
+		t_u8 nibble3 = (c & 0xF000) >> 12;
 
 		dest[0] = '\\';
 		dest[1] = 'u';
-		dest[2] = (hexa3 < 10) ? (hexa3 + '0') : (hexa3 - 10 + 'A');
-		dest[3] = (hexa2 < 10) ? (hexa2 + '0') : (hexa2 - 10 + 'A');
-		dest[4] = (hexa1 < 10) ? (hexa1 + '0') : (hexa1 - 10 + 'A');
-		dest[5] = (hexa0 < 10) ? (hexa0 + '0') : (hexa0 - 10 + 'A');
+		dest[2] = (nibble3 < 10) ? (nibble3 + '0') : (nibble3 - 10 + 'A');
+		dest[3] = (nibble2 < 10) ? (nibble2 + '0') : (nibble2 - 10 + 'A');
+		dest[4] = (nibble1 < 10) ? (nibble1 + '0') : (nibble1 - 10 + 'A');
+		dest[5] = (nibble0 < 10) ? (nibble0 + '0') : (nibble0 - 10 + 'A');
 	}
 	return 6;
 }
@@ -52,25 +52,25 @@ t_size UTF32_ToEscaped_UFFFFFFFF(t_char *dest, t_utf32 c)
 {
 	if (dest)
 	{
-		t_u8 hexa0 = (c & 0x0000000F) >> 0;
-		t_u8 hexa1 = (c & 0x000000F0) >> 4;
-		t_u8 hexa2 = (c & 0x00000F00) >> 8;
-		t_u8 hexa3 = (c & 0x0000F000) >> 12;
-		t_u8 hexa4 = (c & 0x000F0000) >> 16;
-		t_u8 hexa5 = (c & 0x00F00000) >> 20;
-		t_u8 hexa6 = (c & 0x0F000000) >> 24;
-		t_u8 hexa7 = (c & 0xF0000000) >> 28;
+		t_u8 nibble0 = (c & 0x0000000F) >> 0;
+		t_u8 nibble1 = (c & 0x000000F0) >> 4;
+		t_u8 nibble2 = (c & 0x00000F00) >> 8;
+		t_u8 nibble3 = (c & 0x0000F000) >> 12;
+		t_u8 nibble4 = (c & 0x000F0000) >> 16;
+		t_u8 nibble5 = (c & 0x00F00000) >> 20;
+		t_u8 nibble6 = (c & 0x0F000000) >> 24;
+		t_u8 nibble7 = (c & 0xF0000000) >> 28;
 
 		dest[0] = '\\';
 		dest[1] = 'U';
-		dest[2] = (hexa7 < 10) ? (hexa7 + '0') : (hexa7 - 10 + 'A');
-		dest[3] = (hexa6 < 10) ? (hexa6 + '0') : (hexa6 - 10 + 'A');
-		dest[4] = (hexa5 < 10) ? (hexa5 + '0') : (hexa5 - 10 + 'A');
-		dest[5] = (hexa4 < 10) ? (hexa4 + '0') : (hexa4 - 10 + 'A');
-		dest[6] = (hexa3 < 10) ? (hexa3 + '0') : (hexa3 - 10 + 'A');
-		dest[7] = (hexa2 < 10) ? (hexa2 + '0') : (hexa2 - 10 + 'A');
-		dest[8] = (hexa1 < 10) ? (hexa1 + '0') : (hexa1 - 10 + 'A');
-		dest[9] = (hexa0 < 10) ? (hexa0 + '0') : (hexa0 - 10 + 'A');
+		dest[2] = (nibble7 < 10) ? (nibble7 + '0') : (nibble7 - 10 + 'A');
+		dest[3] = (nibble6 < 10) ? (nibble6 + '0') : (nibble6 - 10 + 'A');
+		dest[4] = (nibble5 < 10) ? (nibble5 + '0') : (nibble5 - 10 + 'A');
+		dest[5] = (nibble4 < 10) ? (nibble4 + '0') : (nibble4 - 10 + 'A');
+		dest[6] = (nibble3 < 10) ? (nibble3 + '0') : (nibble3 - 10 + 'A');
+		dest[7] = (nibble2 < 10) ? (nibble2 + '0') : (nibble2 - 10 + 'A');
+		dest[8] = (nibble1 < 10) ? (nibble1 + '0') : (nibble1 - 10 + 'A');
+		dest[9] = (nibble0 < 10) ? (nibble0 + '0') : (nibble0 - 10 + 'A');
 	}
 	return 10;
 }
