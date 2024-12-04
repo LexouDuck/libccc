928887c638f0c68c18274efd2d876d87072c77aa
lexouduck <lexouduck@gmail.com>
2024-12-04 15:51:57 +0900

    feature(libccc/bool): added boolean operators functions and their aliases

--- hdr/libccc/bool.h
+++ hdr/libccc/bool.h
@@ -184,104 +184,118 @@ t_bool					Bool_FromString(t_char const* str);
 
 
 
+//!@doc Boolean operator: ⊤	(always true)
+//!@{
+t_bool				Bool_True(t_bool a, t_bool b);
+#define c_true		Bool_True
+#define Bool_T		Bool_True
+//!@}
+
+//!@doc Boolean operator: ⊥	(always false)
+//!@{
+t_bool				Bool_False(t_bool a, t_bool b);
+#define c_false		Bool_False
+#define Bool_F		Bool_False
+//!@}
+
+//!@doc Boolean operator: ⊣	(left operand value)
+//!@{
+t_bool				Bool_Left(t_bool a, t_bool b);
+#define c_left		Bool_Left
+#define Bool_L		Bool_Left
+//!@}
+
+//!@doc Boolean operator: ⫣	(not left operand value)
+//!@{
+t_bool				Bool_NotLeft(t_bool a, t_bool b);
+#define c_nleft		Bool_NotLeft
+#define Bool_NL		Bool_NotLeft
+//!@}
+
+//!@doc Boolean operator: ⊢	(right operand value)
+//!@{
+t_bool				Bool_Right(t_bool a, t_bool b);
+#define c_right		Bool_Right
+#define Bool_R		Bool_Right
+//!@}
+
+//!@doc Boolean operator: ⊬	(not right operand value)
+//!@{
+t_bool				Bool_NotRight(t_bool a, t_bool b);
+#define c_nright	Bool_NotRight
+#define Bool_NR		Bool_NotRight
+//!@}
+
 //!@doc Boolean operator: ⋀	(and)
 //!@{
-t_bool				Bool_And   (t_bool a, t_bool b);
+t_bool				Bool_And(t_bool a, t_bool b);
 #define c_and		Bool_And
 //!@}
 
 //!@doc Boolean operator: ⊼	(not and)
 //!@{
-t_bool				Bool_NAnd  (t_bool a, t_bool b);
-#define c_nand		Bool_NAnd
+t_bool				Bool_NotAnd(t_bool a, t_bool b);
+#define c_nand		Bool_NotAnd
+#define Bool_NAnd	Bool_NotAnd
 //!@}
 
 //!@doc Boolean operator: ⋁	(or)
 //!@{
-t_bool				Bool_Or    (t_bool a, t_bool b);
+t_bool				Bool_Or(t_bool a, t_bool b);
 #define c_or		Bool_Or
 //!@}
 
 //!@doc Boolean operator: ⊽	(not or)
 //!@{
-t_bool				Bool_NOr   (t_bool a, t_bool b);
-#define c_nor		Bool_NOr
+t_bool				Bool_NotOr(t_bool a, t_bool b);
+#define c_nor		Bool_NotOr
+#define Bool_NOr	Bool_NotOr
 //!@}
 
 //!@doc Boolean operator: ⊻	(exclusive or)
 //!@{
-t_bool				Bool_XOr   (t_bool a, t_bool b);
-#define c_xor		Bool_XOr
+t_bool				Bool_ExOr(t_bool a, t_bool b);
+#define c_xor		Bool_ExOr
+#define Bool_XOr	Bool_ExOr
+#define Bool_NEq	Bool_ExOr
 //!@}
 
 //!@doc Boolean operator: ⇔	(equivalence)
 //!@{
-t_bool				Bool_Eq    (t_bool a, t_bool b);
-#define c_eq		Bool_Eq
+t_bool				Bool_Equiv(t_bool a, t_bool b);
+#define c_eq		Bool_Equiv
+#define Bool_Eq		Bool_Equiv
 //!@}
 
 //!@doc Boolean operator: ⇒	(implication)
 //!@{
-t_bool				Bool_Impl  (t_bool a, t_bool b);
-#define c_impl		Bool_Impl
+t_bool					Bool_Imply(t_bool a, t_bool b);
+#define c_imply			Bool_Imply
+#define Bool_RArrow		Bool_Imply
+//!@}
+
+//!@doc Boolean operator: ⇏	(not implication)
+//!@{
+t_bool					Bool_NotImply(t_bool a, t_bool b);
+#define c_nimply		Bool_NotImply
+#define Bool_NRArrow	Bool_NotImply
+#define Bool_NotRArrow	Bool_NotImply
 //!@}
 
 //!@doc Boolean operator: ⇐	(necessity)
 //!@{
-t_bool				Bool_Need  (t_bool a, t_bool b);
-#define c_need		Bool_Need
-//!@}
-
-/*
-//!@doc Boolean operator: ⇏	(not implication)
-//!@{
-t_bool				Bool_NImpl (t_bool a, t_bool b);
-#define c_nimpl		Bool_NImpl
+t_bool					Bool_Needs(t_bool a, t_bool b);
+#define c_needs			Bool_Needs
+#define Bool_LArrow		Bool_Needs
 //!@}
 
 //!@doc Boolean operator: ⇍	(not necessity)
 //!@{
-t_bool				Bool_NNeed (t_bool a, t_bool b);
-#define c_nneed		Bool_NNeed
+t_bool					Bool_NotNeeds(t_bool a, t_bool b);
+#define c_nneeds		Bool_NotNeeds
+#define Bool_NLArrow	Bool_NotNeeds
+#define Bool_NotLArrow	Bool_NotNeeds
 //!@}
-
-//!@doc Boolean operator: ⊤	(always true)
-//!@{
-t_bool				Bool_True  (t_bool a, t_bool b);
-#define c_true		Bool_True
-//!@}
-
-//!@doc Boolean operator: ⊥	(always false)
-//!@{
-t_bool				Bool_False (t_bool a, t_bool b);
-#define c_false		Bool_False
-//!@}
-
-//!@doc Boolean operator: ⊣	(left operand value)
-//!@{
-t_bool				Bool_Left  (t_bool a, t_bool b);
-#define c_left		Bool_Left
-//!@}
-
-//!@doc Boolean operator: ⫣	(not left operand value)
-//!@{
-t_bool				Bool_Right (t_bool a, t_bool b);
-#define c_right		Bool_Right
-//!@}
-
-//!@doc Boolean operator: ⊢	(right operand value)
-//!@{
-t_bool				Bool_NLeft (t_bool a, t_bool b);
-#define c_nleft		Bool_NLeft
-//!@}
-
-//!@doc Boolean operator: ⊩	(not right operand value)
-//!@{
-t_bool				Bool_NRight(t_bool a, t_bool b);
-#define c_nright	Bool_NRight
-//!@}
-
-*/
 
 
 
---
+++ src/bool/bool.c
@@ -0,0 +1,23 @@
+
+#include "libccc/bool.h"
+
+#include LIBCONFIG_ERROR_INCLUDE
+
+
+
+extern inline t_bool	Bool_True        (t_bool a, t_bool b)	{ return (TRUE); }	 // ⊤
+extern inline t_bool	Bool_False       (t_bool a, t_bool b)	{ return (FALSE); }	 // ⊥
+extern inline t_bool	Bool_Left        (t_bool a, t_bool b)	{ return  (a); }	 // ⊣
+extern inline t_bool	Bool_NotLeft     (t_bool a, t_bool b)	{ return !(a); }	 // ⊢
+extern inline t_bool	Bool_Right       (t_bool a, t_bool b)	{ return  (b); }	 // ⫣
+extern inline t_bool	Bool_NotRight    (t_bool a, t_bool b)	{ return !(b); }	 // ⊩
+extern inline t_bool	Bool_And         (t_bool a, t_bool b)	{ return  (a && b); }	// ⋀
+extern inline t_bool	Bool_NotAnd      (t_bool a, t_bool b)	{ return !(a && b); }	// ⊼
+extern inline t_bool	Bool_Or          (t_bool a, t_bool b)	{ return  (a || b); }	// ⋁
+extern inline t_bool	Bool_NotOr       (t_bool a, t_bool b)	{ return !(a || b); }	// ⊽
+extern inline t_bool	Bool_ExOr        (t_bool a, t_bool b)	{ return  (a != b); }	// ⊻
+extern inline t_bool	Bool_Equiv       (t_bool a, t_bool b)	{ return  (a == b); }	// ⇔
+extern inline t_bool	Bool_Imply       (t_bool a, t_bool b)	{ return  (a == b || b); }	// ⇒
+extern inline t_bool	Bool_NotImply    (t_bool a, t_bool b)	{ return !(a == b || b); }	// ⇏
+extern inline t_bool	Bool_Needs       (t_bool a, t_bool b)	{ return  (a == b || a); }	// ⇐
+extern inline t_bool	Bool_NotNeeds    (t_bool a, t_bool b)	{ return !(a == b || a); }	// ⇍
